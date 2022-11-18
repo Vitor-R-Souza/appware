b3f0fd33c9d1b9c860fdcb40badccb1d401afee1
GGJAVAJS <116683892+GGJAVAJS@users.noreply.github.com>
2022-11-18 17:30:59 -0300

    Update appware.sql

--- appware.sql
+++ appware.sql
@@ -23,7 +23,7 @@
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `categorias` (
   `id_categorias` int NOT NULL AUTO_INCREMENT,
-  `Generos` varchar(60) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `Generos` varchar(60) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci; NOT NULL,
   PRIMARY KEY (`id_categorias`)
 ) ENGINE=InnoDB AUTO_INCREMENT=43 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
