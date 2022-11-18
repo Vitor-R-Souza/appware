5423a6c94eecf9e9246ed9f976ff1a886c800bcd
GGJAVAJS <116683892+GGJAVAJS@users.noreply.github.com>
2022-11-18 16:47:56 -0300

    Update appware.sql

--- appware.sql
+++ appware.sql
@@ -19,7 +19,6 @@
 -- Table structure for table `categorias`
 --
 
-DROP TABLE IF EXISTS `categorias`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `categorias` (
