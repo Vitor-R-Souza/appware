5f49754b3ba544536b6f95d98094df3af31c0efd
geovani-nascto <104122527+geovani-nascto@users.noreply.github.com>
2022-11-18 17:45:55 -0300

    testes e add capas

--- scripts/collection.js
+++ scripts/collection.js
@@ -1,8 +1,84 @@
-var swiper = new Swiper(".popular-content", {
+
+var swiper = new Swiper(".mini-carousel1", {
   slidesPerView:1,
   spaceBetween: 10,
   autoplay: {
     delay: 5500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next", /*.swiper-button-next*/
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+     280:{
+          slidesPerView:1,
+          spaceBetween:10,
+     },
+     320:{
+         slidesPerView:2,
+         spaceBetween:10,
+     },
+     510:{
+       slidesPerView:2,
+       spaceBetween:10,
+     },
+     758:{
+       slidesPerView:3,
+       spaceBetween:15,
+     },
+     900:{
+       slidesPerView:4,
+       spaceBetween:20,
+     },
+  }
+});
+
+  var swiper2 = new Swiper(".mini-carousel2", {
+    slidesPerView:1,
+    spaceBetween: 10,
+    autoplay: {
+      delay: 4500,
+      disableOnInteraction: false,
+    },
+    navigation: {
+      nextEl: ".next2",
+      prevEl: ".prev2",
+    },
+    breakpoints: {
+       280:{
+            slidesPerView:1,
+            spaceBetween:10,
+       },
+       320:{
+           slidesPerView:2,
+           spaceBetween:10,
+       },
+       510:{
+         slidesPerView:2,
+         spaceBetween:10,
+       },
+       758:{
+         slidesPerView:3,
+         spaceBetween:15,
+       },
+       900:{
+         slidesPerView:4,
+         spaceBetween:20,
+       },
+    }
+  });
+
+
+var swiper3 = new Swiper(".mini-carousel3", {
+  slidesPerView:1,
+  spaceBetween: 10,
+  autoplay: {
+    delay: 6500,
     disableOnInteraction: false,
   },
   pagination: {
