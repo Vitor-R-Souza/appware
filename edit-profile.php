16f67f4833bb004c177674b82ce84e5880602923
geovani-nascto <104122527+geovani-nascto@users.noreply.github.com>
2022-11-26 15:52:46 -0300

    perfil que bugou

---
+++ edit-profile.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro</title>
+    <link rel="stylesheet" href="estilos/edit-profile.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" rel="stylesheet">
+    <link rel="shortcut icon" href="imagens/favicon.ico" type="image/x-icon">
+</head>
+
+<body>
+    <?php
+        require_once('requires/connect.php');
+        require_once('requires/func.php');
+    ?>
+    <main>
+        <form action="cadastro-act.php" method="post" class="signin" autocomplete="off">
+        <input type="hidden" name="tipo" value="Dev">
+            <h2>Editar dados</h2>
+            <div class="data-user">
+                <div class="box-user">
+                    <input type="text" name="name" id="name" class="required" oninput="nameValidate()" required>
+                    <label for="name" class="label-error">Nome</label>
+                    <span class="span-required">Nome deve ter no mínimo 3 caracteres</span>
+                </div>
+                <div class="box-user">
+                    <input type="text" name="user" id="user" class="required" oninput="userValidate()" required>
+                    <label for="user" class="label-error">Usuário</label>
+                    <span class="span-required">Mínimo 5 caracteres e separação usar underline</span>
+                </div>
+            </div>
+            <div class="data-user">
+                <div class="box-user">
+                    <input type="text" name="enterprise" id="enterprise" class="required" oninput="enterpriseValidate()" required>
+                    <label for="enterprise" class="label-error">Empresa/Estúdio</label>
+                    <span class="span-required">Nome deve ter no mínimo 3 caracteres</span>
+                </div>
+                <div class="box-user">
+                    <input type="text" name="site-dev" id="site-dev" class="required" oninput="siteValidate()" required>
+                    <label for="site-dev" class="label-error">Site ou Página da Empresa/Estúdio</label>
+                    <span class="span-required">Site deve ter o endereço correto</span>
+                </div>
+            </div>
+            <div class="data-user">
+                <div class="box-user">
+                    <input type="text" name="email" id="email" class="required" oninput="emailValidate()" required>
+                    <label for="email" class="label-error">E-mail</label>
+                    <span class="span-required">Digite um e-mail válido</span>
+                </div>
+                <div class="box-user">
+                    <input type="password" name="senha" id="senha" class="required" oninput="passwordValidate()" required>
+                    <!-- <i class="fa-solid fa-eye" id="btn-eye"></i> -->
+                    <label for="senha" class="label-error">Senha</label>
+                    <img src="imagens/icon-eye-open.png" id="btn-eye">
+                    <span class="span-required">Digite uma senha válida</span>
+                </div>
+            </div>
+
+            <div class="sign-up">
+                <div>
+                    <input type="submit" value="Alterar dados" id="btn">
+                </div>
+            </div>
+        </form>
+    </main>
+    <script src="scripts/edit-profile.js"></script>
+</body>
+
+</html>
---
+++ scripts/edit-profile.js
@@ -0,0 +1,102 @@
+const form = document.getElementById('signin')
+const campos = document.querySelectorAll('.required')
+const spans = document.querySelectorAll('.span-required')
+const labels = document.querySelectorAll('.label-error')
+const emailRegex = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/
+const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#!])[0-9a-zA-Z$*&@#!]{8,}$/
+const userRegex =  /^[a-z\d](?:[a-z\d]|_(?=[a-z\d])){4,14}$/i ///^[A-Za-z]\\w{5,33}$/
+const btnEye = document.querySelector('.btn-eye')
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    nameValidate();
+    userValidate();
+    enterpriseValidate();
+    siteValidate();
+    dateValidate();
+    emailValidate();
+    passwordValidate();
+    comparePassword();
+})
+
+function setError(index){
+    campos[index].style.borderBottom = '2px solid #e63636'
+    spans[index].style.display = 'block'
+    labels[index].style.color = 'red'
+}
+
+function removeError(index){
+    campos[index].style.borderBottom = ''
+    spans[index].style.display = 'none'
+    labels[index].style.color = ''
+}
+
+
+function nameValidate(){
+    if(campos[0].value.length == ''){
+        removeError(0)
+    } else if(campos[0].value.length < 3){
+        setError(0)
+    }else{
+        removeError(0)
+    }
+}
+
+function userValidate(){
+    if(campos[1].value.length == ''){
+        removeError(1)
+    } else if(!userRegex.test(campos[1].value)){
+        setError(1)
+        console.log("Usuário invalido")
+    }else{
+        removeError(1)
+        console.log("Usuario valido")
+    }
+}
+
+function enterpriseValidate(){
+    if(campos[2].value.length == ''){
+        removeError(2)
+    } else if(campos[2].value.length < 5){
+        setError(2)
+    }else{
+        removeError(2)
+    }
+}
+
+function siteValidate(){
+    if(campos[3].value.length == ''){
+        removeError(3)
+    } else if(campos[3].value.length < 12){
+        setError(3)
+    }else{
+        removeError(3)
+    }
+}
+
+function emailValidate(){
+    if(campos[4].value.length == ''){
+        removeError(4)
+    } else if(!emailRegex.test(campos[4].value)){
+        setError(4)
+    }else{
+        removeError(4)
+    }
+}
+
+function passwordValidate(){
+    if(campos[5].value.length == ''){
+        removeError(5)
+    } else if(!passwordRegex.test(campos[5].value)){
+        setError(5)
+    } else {
+        removeError(5)
+    }
+
+    document.getElementById('btn-eye').addEventListener('click', function(){
+        document.getElementById('senha').type = 'text'
+    })
+    document.getElementById('btn-eye').addEventListener('mouseout', function(){
+        document.getElementById('senha').type = 'password'
+    })
+}
