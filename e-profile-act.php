be38ab684ce354eae0842f15f6cd416958bdb44b
vrds022 <101133891+vrds022@users.noreply.github.com>
2022-12-01 15:22:25 -0300

    att alt dev profile

--- e-profile-act.php
+++ e-profile-act.php
@@ -4,22 +4,22 @@
     
     extract($_POST);
 
-    if(is_dev() or is_adm()){
-        if(!isset($senha)){
-            $senhaF = gerarHash($senha);
-
-            $BD->query("UPDATE `usuarios` SET `nome` = '$name', `nome_usuario` = '$user', `email` = '$email', `empresa` = '$enterprise', `site_empresa` = '$site_dev', `senha` = '$senhaF' WHERE `usuarios`.`id_usuario` = $id");
-        }else{
-            $BD->query("UPDATE `usuarios` SET `nome` = '$name', `nome_usuario` = '$user', `email` = '$email', `empresa` = '$enterprise', `site_empresa` = '$site_dev' WHERE `usuarios`.`id_usuario` = $id");
-        }
-        header("location:edit-profile.php");
-    }else{
+    if(is_user()){
         if(!isset($senha)){
             $senhaF = gerarHash($senha);
 
             $BD->query("UPDATE `usuarios` SET `nome` = '$name', `nome_usuario` = '$user', `email` = '$email', `senha` = '$senhaF' WHERE `usuarios`.`id_usuario` = $id");
         }else{
             $BD->query("UPDATE `usuarios` SET `nome` = '$name', `nome_usuario` = '$user', `email` = '$email' WHERE `usuarios`.`id_usuario` = $id");
+        }
+        header("location:edit-profile.php");
+    }else{
+        if(!isset($senha)){
+            $senhaF = gerarHash($senha);
+
+            $BD->query("UPDATE `usuarios` SET `nome` = '$name', `nome_usuario` = '$user', `email` = '$email', `empresa` = '$enterprise', `site_empresa` = '$site', `senha` = '$senhaF' WHERE `usuarios`.`id_usuario` = $id");
+        }else{
+            $BD->query("UPDATE `usuarios` SET `nome` = '$name', `nome_usuario` = '$user', `email` = '$email', `empresa` = '$enterprise', `site_empresa` = '$site' WHERE `usuarios`.`id_usuario` = $id");
         }
         header("location:edit-profile.php");
     }
