c31bd85b227ffe8914db7460a57f7cf2086b0639
geovani-nascto <104122527+geovani-nascto@users.noreply.github.com>
2022-12-07 11:55:30 -0300

    sobre

--- about.php
+++ about.php
@@ -17,10 +17,24 @@
     ?>
     <main>
         <section class="about-origin">
-
+            <div class="origin">
+                <h1>Como surgiu o AppWare?</h1>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam rerum optio aut necessitatibus, illum velit dolore perferendis deserunt accusamus excepturi nulla ad modi laborum eos aliquid beatae aperiam iste in?</p>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Atque itaque officiis veniam ut voluptatum! Odio id quos ratione? Doloribus inventore praesentium quis? Consectetur amet ab ex voluptatem sapiente, cumque similique.</p>
+            </div>
+            <div class="logo-enterprise">
+                <img src="imagens/ldr-technology.png" alt="Logo da LDR Technology">
+            </div>
         </section>
         <section class="about-objective">
-            
+            <div class="ware">
+                <img src="imagens/ware-mascote.png" alt="Mascote do AppWare">
+            </div>
+            <div class="objective">
+                <h1>Para que serve o AppWare?</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente voluptatem possimus pariatur fugiat eveniet tenetur ipsam. Odio, dolor amet itaque placeat ducimus natus in pariatur consectetur perspiciatis facilis assumenda illo.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta quo sequi odit tenetur ab quam! Reiciendis, similique! Unde perferendis voluptas, quaerat iure libero eos reiciendis ea odio nulla tempore minus.</p>
+            </div>
         </section>
     </main>
     <?php
